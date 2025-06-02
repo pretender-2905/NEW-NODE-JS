a7764ebec565540ac745eadaaf7db95a061ae8af
pretender-2905 <pretender2905@gmail.com>
2025-06-03 03:01:21 +0500

    commits

--- CRUD/index.js
+++ CRUD/index.js
@@ -23,18 +23,36 @@ const users = [
 
 
 import express from 'express'
+import morgan from 'morgan'
 const app = express()
-app.use(express.json())
+app.use(express.json())  //also a middleware (post ki request se jab body mai data bhejty hain to ye osy json mai convert kr deta hai)
 const PORT = 3000
 
 
-app.get("/", (req,res)=>{
+// middleware application level
+app.use(morgan('tiny'))    //morgan use krty hian check krny ke lia ke konsi api call ho rhi hai
+
+// application level middleware
+
+function middleware(req, res, next){
+    // console.log("Middleware", Date.now());   //eg of executing any code
+    req.requestBy = "Muhammad Ibrahim"   //eg of doing changes in req/res
+    res.status(500).send("System mai maslah ho gya bhai!!!!") // eg of ending req.res cycle!!!
+    next();
+}
+
+// app.use(middleware);
+
+app.get("/", middleware,  (req,res)=>{
     res.send("HELLO FROM NEW NODE..... I LOVE YOU")
+    console.log("Request by --> ", req.requestBy)
 })
 app.post("/", (req,res)=>{
+    console.log("req.body->", req.body)
     res.send(users)
 })
 
 app.listen(PORT, ()=>{
     console.log(`The server is running on http://localhost:${PORT}`)
 })
+
